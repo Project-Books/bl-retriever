ec0f6354d87c506213e3c1f855d0c223f12a2de6
Karan <karan.n.j.kumar@gmail.com>
2021-10-15 19:48:11 +0100

    Use DictReader

--- src/main.py
+++ src/main.py
@@ -3,10 +3,28 @@ import csv
 
 def read_csv():
     with open('../datasets/roald-dahl/records.csv', encoding='utf-8', newline='') as csvfile:
-        rows = csv.reader(csvfile, delimiter=' ',quotechar='|')
+        rows = csv.DictReader(csvfile)
         print('Row: ' + str(rows))
         for row in rows:
-            print(', '.join(row))
+            print(
+                row['Content type'],
+                row['Material type'],
+                row['ISBN'],
+                row['Name'],
+                row['Role'],
+                row['All names'],
+                row['Title'],
+                row['Variant titles'],
+                row['Series title'],
+                row['Country of publication'],
+                row['Publisher'],
+                row['Date of creation/publication'],
+                row['Edition'],
+                row['Topics'],
+                row['Genre'],
+                row['Languages'],
+                row['Notes'],
+            )
 
 
 if __name__ == '__main__':
